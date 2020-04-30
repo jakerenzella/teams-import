f8cd536055b1a7e9c6fa1bfccf5031e431b7c92c
Jake Renzella <j@kerenzella.com>
2020-04-30 13:51:52 +1000

    QUALITY: Remove generated file

--- import.ps1
+++
@@ -1,4 +0,0 @@
-Import-Module MicrosoftTeams
-Connect-MicrosoftTeams
-Get-Team -User jake.renzella@deakin.edu.au | Select-Object GroupID, DisplayName | Export-CSV -path ./teams.csv -NoTypeInformation
-Import-Csv -Path ./csvs/SIT102.csv | ForEach-Object {Add-TeamUser -GroupId 2f99227c-3191-474d-910c-6a35a71fa2be -user $_.email}
